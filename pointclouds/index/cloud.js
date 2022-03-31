ae448ea4882eedb1179b4cd3590a4380d20cfe03
Alejandro <faustoaguanor@gmail.com>
2022-03-30 20:05:10 -0500

    adding files

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=15 +south +datum=WGS84 +units=m +no_defs",
+    "points": 4895819,
+    "boundingBox": {
+        "lx": 709133.67,
+        "ly": 9913161.22,
+        "lz": 883.51,
+        "ux": 710163.73,
+        "uy": 9914191.280000002,
+        "uz": 1913.5699999999395
+    },
+    "tightBoundingBox": {
+        "lx": 709133.67,
+        "ly": 9913161.22,
+        "lz": 883.51,
+        "ux": 710163.73,
+        "uy": 9913886.8,
+        "uz": 1011.1800000000001
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 8.920580863952637,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
